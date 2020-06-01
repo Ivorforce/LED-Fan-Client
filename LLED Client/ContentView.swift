ee093112b749835a34bc4b0ee822a7624c40a4b9
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2020-06-01 20:40:27 +0200

    Initial Commit

---
+++ LLED Client/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LLED Client
+//
+//  Created by Lukas Tenbrink on 01.06.20.
+//  Copyright © 2020 Lukas Tenbrink. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
