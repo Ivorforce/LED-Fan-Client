d145a0429b060fc1c8ff148ffc04e11a822d5707
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2020-06-02 01:54:47 +0200

    Also attempt to connect on load.

--- LLED Client/ServerInfo.swift
+++ LLED Client/ServerInfo.swift
@@ -13,47 +13,51 @@ class ServerInfo: ObservableObject {
         case invalidURL, noConnection, connecting, connected
     }
     
+    init() {
+        connect()
+    }
+    
     var urlString: String = UserDefaults.standard.string(forKey: "server") ?? "" {
         didSet {
             UserDefaults.standard.set(urlString, forKey: "server")
-            url = URL(string: "http://\(urlString)")
+            connect()
         }
     }
     
+    var url: URL? { URL(string: "http://\(urlString)") }
+
     var state: State = .invalidURL {
         willSet {
             self.objectWillChange.send()
         }
     }
     
-    var url: URL? = nil {
-        didSet {
-            guard let url = url else {
-                state = .invalidURL
-                return
-            }
-            
-            state = .connecting
-
-            let task = URLSession.shared.dataTask(with: url) { [unowned self] (data, response, error) in
-                DispatchQueue.main.sync {
-                    guard
-                        let data = data,
-                        let json =  try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-                        self._interpret(info: json)
-                    else {
-                        self.state = .noConnection
-                        return
-                    }
-                    
-                    self.state = .connected
-                }
-            }
-
-            task.resume()
+    func connect() {
+        guard let url = url else {
+            state = .invalidURL
+            return
         }
+        
+        state = .connecting
+
+        let task = URLSession.shared.dataTask(with: url) { [unowned self] (data, response, error) in
+            DispatchQueue.main.sync {
+                guard
+                    let data = data,
+                    let json =  try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                    self._interpret(info: json)
+                else {
+                    self.state = .noConnection
+                    return
+                }
+                
+                self.state = .connected
+            }
+        }
+
+        task.resume()
     }
-    
+        
     func _interpret(info: [String: Any]) -> Bool {
         return false
     }
